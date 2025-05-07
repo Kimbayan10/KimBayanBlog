0815c6eea6fece064c388226414524f347d9af7e
Kimbayan10 <skykimbayan@gmail.com>
2025-05-08 04:52:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// This is just an example for smooth scrolling behavior
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
